92553ce501afc7963a433e070bf461ee604222ae
jefflingit <jeff91304b@gmail.com>
2024-12-17 18:51:21 +0800

    test for commit to master

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("test for commit 4");
+		System.out.println("test for commit to master");
 	}
 
 }
