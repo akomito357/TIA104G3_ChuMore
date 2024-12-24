0b4b94e6403ed539eb1b93c6559709e61db30897
Ariel Chien <jacinth170625@gmail.com>
2024-12-25 02:14:19 +0800

    add FavRest models and other Repositories; fix bugs in OrderMasterVO

---
+++ src/main/java/com/chumore/favrest/model/FavRestServiceImpl.java
@@ -0,0 +1,34 @@
+package com.chumore.favrest.model;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.chumore.member.model.MemberVO;
+
+@Service("favRestService")
+public class FavRestServiceImpl implements FavRestService{
+
+	@Autowired
+	FavRestRepository repository;
+	
+	@Override
+	public void addFavRest(FavRestVO favRest) {
+		repository.save(favRest);
+	}
+
+	@Override
+	public void deleteFavRest(Integer favRestId) {
+		if(repository.existsById(favRestId)) {
+			repository.deleteByFavRestId(favRestId);
+		}
+	}
+
+	@Override
+	public List<String> getRestByMember(MemberVO member) {
+		return repository.findRestByMember(member);
+	}
+	
+
+}
