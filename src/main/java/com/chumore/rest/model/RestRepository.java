20e066df9c397c8e2b1414a99be3f7708579fedf
Ariel Chien <jacinth170625@gmail.com>
2024-12-25 15:50:01 +0800

    add rest.compositequery package and modify RestRepository

--- src/main/java/com/chumore/rest/model/RestRepository.java
+++ src/main/java/com/chumore/rest/model/RestRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface RestRepository extends JpaRepository<RestVO, Integer>{
 	
-	@Query(value = "from RestVO r where r.city = ?1 and r.district = ?2")
+	@Query(value = "from RestVO r where r.restCity = ?1 and r.restDist = ?2")
 	List<RestVO> findByCityAndDist(String city, String district);
 	
 
