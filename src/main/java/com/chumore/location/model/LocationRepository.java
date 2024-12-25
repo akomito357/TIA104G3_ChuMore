6a02414133aed5f0fb1c5bd8a5081150127b35f6
Ariel Chien <jacinth170625@gmail.com>
2024-12-25 11:20:25 +0800

    fix bugs: solved conflicts in LocationRepository

--- src/main/java/com/chumore/location/model/LocationRepository.java
+++ src/main/java/com/chumore/location/model/LocationRepository.java
@@ -9,12 +9,6 @@ public interface LocationRepository extends JpaRepository<LocationVO, Integer>{
 
 	@Query("select l.district from LocationVO l where l.city = ?1")
 	List<String> findDistByCity(String city);
-<<<<<<< Upstream, based on branch 'master' of https://github.com/akomito357/TIA104G3_ChuMore.git
-	
-	@Query(value = "select l.city from LocationVO l where l.city = ?1")
-	List<String> findCity(String city);
-=======
->>>>>>> 66f000b fix: resolve the conflict due to configuration of hibernate and spring data jpa
 	
 	@Query(value = "select l.city from LocationVO l where l.city = ?1")
 	List<String> findCity(String city);
