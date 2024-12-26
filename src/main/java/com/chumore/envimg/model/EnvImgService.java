0d97ab77b12205f7c3893728f38045faa801d1d7
ChenXunChang <jason86118@gmail.com>
2024-12-26 19:04:20 +0800

    1.envimg&menuimg 多張圖片上傳功能成功(但與hibernate config有衝突)、待測刪除功能 2.增加blobreaderController(老師的)，待測試(用於前端拿取資料庫圖片使用)

--- src/main/java/com/chumore/envimg/model/EnvImgService.java
+++ src/main/java/com/chumore/envimg/model/EnvImgService.java
@@ -1,37 +1,26 @@
 package com.chumore.envimg.model;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service("envImgService")
 public class EnvImgService {
 	@Autowired
 	EnvImgRepository repository;
 
-	// 新增單一環境圖片
-	@Transactional(transactionManager = "jpaTransactionManager")
+	// 新增環境圖片
+	
 	public void addEnvImg(EnvImgVO envImg) {
 		repository.save(envImg);
 	}
 
 	// 新增多張環境圖片
-	@Transactional(transactionManager = "jpaTransactionManager")
-	public void addMultipleEnvImgs(MultipartFile file, Integer restId) {
-		try {
-			EnvImgVO envImg = new EnvImgVO();
-			envImg.setImage(file.getBytes());
-			envImg.getRest().getRestId();
-			repository.save(envImg);
-		} catch (IOException e) {
-			throw new RuntimeException("Failed to save menu image", e);
-		}
-	}
+	public void addMultipleEnvImgs(EnvImgVO envImg) {
+		repository.save(envImg);
+	}	
 
 	// 修改環境圖片
 	public void updateEnvImg(EnvImgVO envImg) {
--- src/main/java/com/chumore/menuimg/controller/MenuImgController.java
+++ src/main/java/com/chumore/menuimg/controller/MenuImgController.java
@@ -1,4 +1,4 @@
-package com.chumore.envimg.controller;
+package com.chumore.menuimg.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,65 +21,66 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.chumore.envimg.model.EnvImgService;
-import com.chumore.envimg.model.EnvImgVO;
+
+import com.chumore.menuimg.model.MenuImgService;
+import com.chumore.menuimg.model.MenuImgVO;
 import com.chumore.rest.model.RestVO;
 
 
 
 
 @Controller
-@RequestMapping("/envImg")
+@RequestMapping("/menuImg")
 @MultipartConfig
-public class EnvImgController {
+public class MenuImgController {
 	
 	@Autowired
-	EnvImgService envImgSvc;
+	MenuImgService menuImgSvc;
 	
 	
-	@PostMapping("addEnvImg")
-	public String addEnvImg(@Valid EnvImgVO envImg, BindingResult result, ModelMap model,
+	@PostMapping("addMenuImg")
+	public String addMenuImg(@Valid MenuImgVO menuImg, BindingResult result, ModelMap model,
 			@RequestParam("upFiles") MultipartFile[] parts) throws IOException {
 
 		/*************************** 1.接收請求參數 - 輸入格式的錯誤處理 ************************/
 		// 去除BindingResult中upFiles欄位的FieldError紀錄 --> 見第172行
-		result = removeFieldError(envImg , result, "upFiles");
+		result = removeFieldError(menuImg , result, "upFiles");
 
 		if (parts[0].isEmpty()) { // 使用者未選擇要上傳的圖片時
 			model.addAttribute("errorMessage", "員工照片: 請上傳照片");
 		} else {
 			
 			RestVO rest = new RestVO();
-			rest.setRestId(2001);
+			rest.setRestId(2001);//先預設一個restId之後要從關聯式資料庫帶入
 				
 			for (MultipartFile multipartFile : parts) {
 				byte[] buf = multipartFile.getBytes();
 				
-				EnvImgVO img = new EnvImgVO();
+				MenuImgVO img = new MenuImgVO();
 				img.setImage(buf);	
 				img.setRest(rest);
 				/*************************** 2.開始新增資料 *****************************************/
-				envImgSvc.addEnvImg(img);
+				menuImgSvc.addMenuImg(img);
 				
 			}
 		}
 		if (result.hasErrors() || parts[0].isEmpty()) {
-			return "/secure/rest/resturant_registration_form";
+			return "index";
 		}
 		/*************************** 3.新增完成,準備轉交(Send the Success view) **************/
-		List<EnvImgVO> envImgList = envImgSvc.getAll();
-		model.addAttribute("envImg", envImgList); // 傳遞到前端
+		List<MenuImgVO> menuImgList = menuImgSvc.getAll();
+		model.addAttribute("menuImg", menuImgList); // 傳遞到前端
 //		model.addAttribute("Succesful", "- (新增成功)");
 		return "/secure/rest/resturant_registration_form"; 
 	}
 	
 	@PostMapping("delete")
-	public String delete(@RequestParam("emv_img_id") String envImgId, ModelMap model) {
-		EnvImgService empSvc = new EnvImgService();
-		empSvc.deleteEnvImg(Integer.valueOf(envImgId));
+	public String delete(@RequestParam("menu_img_id") String menuImgId, ModelMap model) {
+		MenuImgService menuImgSvc = new MenuImgService();
+		menuImgSvc.deleteMenuImg(Integer.valueOf(menuImgId));
 		
-		List<EnvImgVO> envImgList = envImgSvc.getAll();
-		model.addAttribute("envImg",envImgList); 
+		List<MenuImgVO> menuImgList = menuImgSvc.getAll();
+		model.addAttribute("menuImg",menuImgList); 
 		
 		return "/secure/rest/resturant_registration_form"; //重導回原本頁面
 	}
@@ -87,17 +88,17 @@ public class EnvImgController {
 	@GetMapping("getAll")
 	public String getAll(Model model) {
 		
-		List<EnvImgVO> envImg = envImgSvc.getAll();
-		model.addAttribute("envImgs", envImg);
+		List<MenuImgVO> menuImgList = menuImgSvc.getAll();
+		model.addAttribute("menuImg",menuImgList); 
 		return "/secure/rest/resturant_registration_form"; 
 	} 
 		
 	// 去除BindingResult中某個欄位的FieldError紀錄
-	public BindingResult removeFieldError(EnvImgVO envImgVO, BindingResult result, String removedFieldname) {
+	public BindingResult removeFieldError(MenuImgVO menuImgVO, BindingResult result, String removedFieldname) {
 		List<FieldError> errorsListToKeep = result.getFieldErrors().stream()
 				.filter(fieldname -> !fieldname.getField().equals(removedFieldname))
 				.collect(Collectors.toList());
-		result = new BeanPropertyBindingResult(envImgVO, "envImgVO");
+		result = new BeanPropertyBindingResult(menuImgVO, "envImgVO");
 		for (FieldError fieldError : errorsListToKeep) {
 			result.addError(fieldError);
 		}
