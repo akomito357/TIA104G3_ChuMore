045a2befed3e5d0e0122a0f47be872fd3fe59cb3
Ruby Hsu <orangezo6zo6@gmail.com>
2024-12-27 23:28:11 +0800

    add controller for OrderLineItem and ProductCategory

--- src/main/java/com/chumore/productcategory/model/ProductCategoryRepository.java
+++ src/main/java/com/chumore/productcategory/model/ProductCategoryRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ProductCategoryRepository extends JpaRepository<ProductCategoryVO, Integer>{
-	
+	@Query("FROM ProductCategoryVO pc WHERE pc.restId = :restId")
+	List<ProductCategoryVO> getCategoryListByRest(Integer restId);
 }
