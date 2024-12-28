3f08986bc9a830184beb194f65b808d8b5d3cb58
Ariel Chien <jacinth170625@gmail.com>
2024-12-28 15:42:55 +0800

    refactor: move ReviewImg models to independent package.

--- src/main/java/com/chumore/review/controller/ReviewController.java
+++ src/main/java/com/chumore/review/controller/ReviewController.java
@@ -1,20 +1,29 @@
 package com.chumore.review.controller;
 
-import com.chumore.review.model.ReviewImageService;
-import com.chumore.review.model.ReviewImageVO;
-import com.chumore.review.model.ReviewService;
-import com.chumore.review.model.ReviewVO;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.chumore.review.model.ReviewService;
+import com.chumore.review.model.ReviewVO;
+import com.chumore.reviewimg.model.ReviewImageService;
+import com.chumore.reviewimg.model.ReviewImageVO;
+
+@Controller
 @RequestMapping("/reviews")
 public class ReviewController {
 
--- src/main/java/com/chumore/reviewimg/model/ReviewImageVO.java
+++ src/main/java/com/chumore/reviewimg/model/ReviewImageVO.java
@@ -1,9 +1,20 @@
-package com.chumore.review.model;
+package com.chumore.reviewimg.model;
 
-import javax.persistence.*;
-import javax.validation.constraints.*;
 import java.io.Serializable;
 import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.chumore.review.model.ReviewVO;
 
 @Entity
 @Table(name = "review_image")
