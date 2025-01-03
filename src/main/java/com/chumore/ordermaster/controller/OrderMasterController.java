99bf786aee20b0599f9d774c675d20e8eebbb26d
akomito357 <149935076+akomito357@users.noreply.github.com>
2025-01-04 01:03:30 +0800

    Update OrderMasterController.java

--- src/main/java/com/chumore/ordermaster/controller/OrderMasterController.java
+++ src/main/java/com/chumore/ordermaster/controller/OrderMasterController.java
@@ -13,19 +13,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.chumore.ordermaster.model.OrderMasterService;
 import com.chumore.ordermaster.model.OrderMasterVO;
 import com.chumore.ordermaster.res.OrderMasterResponse;
 
-@CrossOrigin // for dev
 @Controller
 @RequestMapping("/orders")
 public class OrderMasterController {
@@ -34,33 +31,18 @@ public class OrderMasterController {
 	OrderMasterService orderSvc;
 
 	// getOneForCheckOut / getOneForUpdate（連結到商家結帳確認頁面）
-	@GetMapping("getOne")
-	public String getOneForCheckOut(HttpSession session, Model model) {
+	@PostMapping("getOne")
+	public String getOneForCheckOut(@RequestParam("orderId") String orderId, Model model) {
 		// 之後要再把id改成從session取得
-//		OrderMasterVO orderMaster = orderSvc.getOneById(Integer.valueOf(orderId));
-//		OrderMasterVO orderMaster = orderSvc.getOneById(1);
-		session.setAttribute("orderId", 1);
-		OrderMasterVO orderMaster = orderSvc.getOneById((Integer)session.getAttribute("orderId"));
+		OrderMasterVO orderMaster = orderSvc.getOneById(Integer.valueOf(orderId));
 		model.addAttribute("orderMaster", orderMaster);		
-		return "secure/rest/order/order_page";
+		return "";
 	}
 	
-	// getOneOrder (RESTful)
-	@GetMapping("findOneByOrderId")
-	@ResponseBody
-	public ResponseEntity<OrderMasterResponse> findOneOrder(@RequestParam Integer orderId){
-		OrderMasterResponse<OrderMasterVO> res = null;
-		OrderMasterVO orderMaster = null;
-		try {
-//			Integer orderId = req.get("orderId");
-			orderMaster = orderSvc.getOneById(orderId);
-			res = new OrderMasterResponse<>("success", 200, orderMaster);
-			return ResponseEntity.ok(res);
-		}catch (Exception e){
-			res = new OrderMasterResponse<>("error", 400, orderMaster);
-			return ResponseEntity.badRequest().body(res);
-		}
-	}
+//	// getOneOrder (RESTful)
+//	public ResponseEntity<OrderMasterResponse> getOneOrder(@RequestBody Map<String, Integer>, ){
+//		
+//	}
 	
 	
 	// updateOrder
@@ -123,6 +105,12 @@ public class OrderMasterController {
 		model.addAttribute("memberOrderList", list);
 		// 3. 準備轉交
 		return "";
+	}
+	
+	@GetMapping("test")
+	public String getOneForOrder(HttpSession session, ModelMap map) {
+		session.setAttribute("orderId", 1);
+		return "/secure/rest/order/working_order_page";
 	}
 	
 }
