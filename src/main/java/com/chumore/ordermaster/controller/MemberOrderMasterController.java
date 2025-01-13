046a6443ba1f28a43c2d8744a45843f62bdc10dc
Ruby Hsu <orangezo6zo6@gmail.com>
2025-01-13 23:36:54 +0800

    網址暫保持原樣

--- src/main/java/com/chumore/ordermaster/controller/MemberOrderMasterController.java
+++ src/main/java/com/chumore/ordermaster/controller/MemberOrderMasterController.java
@@ -95,8 +95,8 @@ public class MemberOrderMasterController {
 		return ResponseEntity.ok(response);
 	}
 
-	@GetMapping("dining/history")
-	public String memberDiningHistory() {
-		return "secure/member/dining/member_dining_history";
-	}
+//	@GetMapping("dining/history")
+//	public String memberDiningHistory() {
+//		return "secure/member/dining/member_dining_history";
+//	}
 }
