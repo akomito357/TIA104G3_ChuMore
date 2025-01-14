9d9779c6f158f4efa0e223ca2f707acd1e81c552
jefflingit <jeff91304b@gmail.com>
2025-01-14 21:02:54 +0800

    feat: Enhance Reservation and Daily Reservation Controllers with New Features and Remove Deprecated Code

--- src/main/java/com/chumore/rest/model/RestRepository.java
+++ src/main/java/com/chumore/rest/model/RestRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RestRepository extends JpaRepository<RestVO, Integer>{
 	
@@ -15,6 +16,15 @@ public interface RestRepository extends JpaRepository<RestVO, Integer>{
     Optional<RestVO> findByMerchantEmail(String merchantEmail);
     boolean existsByMerchantEmail(String merchantEmail);
     boolean existsByPhoneNumber(String phoneNumber);
-	
+
+    @Query("SELECT r.restId FROM RestVO r " +
+            "WHERE (:city IS NULL OR r.restCity = :city) " +
+            "AND (:district IS NULL OR r.restDist = :district) " +
+            "AND (:cuisineTypeId IS NULL OR r.cuisineType.cuisineTypeId = :cuisineTypeId)")
+    List<Integer> findRestIdsByOptionalFields(
+            @Param("city") String city,
+            @Param("district") String district,
+            @Param("cuisineTypeId") Integer cuisineTypeId
+    );
 
 }
