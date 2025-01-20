ba1244a48de53c42ec92360fd022c13143dcf518
akomito357 <ariel860115@gmail.com>
2025-01-20 14:01:27 +0800

    fix: member's dining history page error

--- src/main/java/com/chumore/discpts/controller/DiscPtsController.java
+++ src/main/java/com/chumore/discpts/controller/DiscPtsController.java
@@ -1,23 +1,17 @@
 package com.chumore.discpts.controller;
 
 import java.util.List;
-import java.util.Map;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.chumore.discpts.dto.DiscPtsSummaryDTO;
 import com.chumore.discpts.model.DiscPtsService;
-import com.chumore.discpts.model.DiscPtsVO;
-import com.chumore.util.ResponseUtil;
 
 
 @Controller
@@ -29,8 +23,9 @@ public class DiscPtsController {
     
     @GetMapping("/points")
     public String getPointsSummary(
-            @RequestParam Integer memberId,
-            Model model) {
+//            @RequestParam Integer memberId,
+            Model model, HttpSession session) {
+    	Integer memberId = (Integer)session.getAttribute("memberId");
         List<DiscPtsSummaryDTO> summary = discPtsService.getPointsSummary(memberId);
         model.addAttribute("pointsSummary", summary);
         return "member/points";
