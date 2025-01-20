562dff453b6ed50040edb68ee8a7af614b848b8d
akomito357 <ariel860115@gmail.com>
2025-01-20 16:06:27 +0800

    fix: errors

--- src/main/java/com/IndexController.java
+++ src/main/java/com/IndexController.java
@@ -1,11 +1,22 @@
 package com;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.chumore.rest.model.RestService;
+import com.chumore.rest.model.RestVO;
+import com.chumore.util.ResponseUtil;
 
 
 @Controller
@@ -15,6 +26,9 @@ public class IndexController {
 	 * 這支class除了引導到首頁外，可以用來測試已引入thymeleaf的html，
 	 * 請自由新增欲測試的頁面。但請注意有一定機率導致git衝突。
 	 */
+	
+	@Autowired
+	RestService restSvc;
 
 	@GetMapping("/")
 	public String index() {
@@ -57,6 +71,14 @@ public class IndexController {
 	@GetMapping("/searchPage")
 	public String login() {
 		return "search_result";
+	}
+	
+	@PostMapping("/getRandomRest")
+	@ResponseBody
+	public ResponseEntity<?> findRandomRest(@RequestBody Map<String, Integer> map){
+		List<RestVO> list = restSvc.getRandomRests(map.get("count"));
+		ResponseUtil res = new ResponseUtil("success", 200, list);
+		return ResponseEntity.ok(res);
 	}
 	
 }
--- src/main/java/com/chumore/rest/model/RestRepository.java
+++ src/main/java/com/chumore/rest/model/RestRepository.java
@@ -24,5 +24,8 @@ public interface RestRepository extends JpaRepository<RestVO, Integer> {
     @Query("SELECT r FROM RestVO r WHERE r.merchantEmail = :merchantEmail")
     Optional<RestVO> findByMerchantEmailWithLog(@Param("merchantEmail") String merchantEmail);
 
+    @Query(value = "select * from chumore.rest where business_status=1 order by rand() limit ?1;", nativeQuery = true)
+    List<RestVO> findRandomRest(Integer randomCount);
+    
 }
 
