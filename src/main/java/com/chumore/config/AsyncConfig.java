8475831fdb3d6fe76ae414b296e42d5078b33ec9
jefflingit <jeff91304b@gmail.com>
2025-01-21 23:34:33 +0800

    fix: add enable async to the config

--- src/main/java/com/chumore/config/AsyncConfig.java
+++ src/main/java/com/chumore/config/AsyncConfig.java
@@ -2,12 +2,15 @@ package com.chumore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 @Configuration
+@EnableAsync
 public class AsyncConfig {
+
 
     @Bean(name = "taskExecutor") // 指定 Bean 名稱
     public Executor taskExecutor() {
